705780cd8cfb3517b24d5aaf2c2273ce0f7d008d
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-07-08 14:15:56 -0500

    Move conftest.py out of root

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+import pytest
+
+from pullapprove.utils import json_load
+
+
+@pytest.fixture
+def testdata_objs():
+    # uses a custon json.load that encodes dates
+    return lambda name: json_load(
+        open(os.path.join(os.path.dirname(__file__), "testdata", name + ".json")).read()
+    )
+
+
+@pytest.fixture
+def testdata():
+    return lambda name: json.load(
+        open(os.path.join(os.path.dirname(__file__), "testdata", name + ".json"))
+    )
