5e768b04692d58ea6c0d802baa64063b8858bea7
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-11-29 16:11:56 -0600

    Add Redis cache option for API calls (#27)

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,20 @@ def testdata():
     return lambda name: json.load(
         open(os.path.join(os.path.dirname(__file__), "testdata", name + ".json"))
     )
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--liveapi",
+        action="store_true",
+        dest="liveapi",
+        default=False,
+        help="enable liveapi decorated tests",
+    )
+
+
+def pytest_configure(config):
+    # By default, liveapi tests are disabled
+    # add --liveapi to run ALL tests
+    if not config.option.liveapi:
+        setattr(config.option, "markexpr", "not liveapi")
